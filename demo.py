f11fdcffed15429318e23efc6e379c0cf93eb3ec
chris <chris.kind.man@gmail.com>
2022-02-01 23:48:03 +0000

    Add demo for bindings

---
+++ demo.py
@@ -0,0 +1,165 @@
+import wiring_rs
+import json
+
+
+def thick_2_ofn_demo():
+    triple = {
+        "subject": "obo:OBI_0001636",
+        "predicate": "rdfs:subClassOf",
+        "object": {
+            "owl:someValuesFrom": [{"object": "obo:OBI_0500000"}],
+            "rdf:type": [{"object": "owl:Restriction"}],
+            "owl:onProperty": [{"object": "obo:BFO_0000050"}],
+        },
+    }
+    triple_string = json.dumps(triple)
+    print("======")
+    print("DEMO: Thick to OFN-S expression")
+    print("-------------------------------")
+    print("Input: ")
+    print(triple_string)
+    print("Output: ")
+    print(wiring_rs.thick_2_ofn(triple_string))
+    print("======")
+
+
+def typing_demo():
+
+    # The handling of quotes is somewhat ugly.
+    # Wiring uses JSON under the hood.
+    # So, '"s"' is a string containing a JSON string
+    m = {
+        '"obo:CHEBI_33262"': {'"owl:Class"'},
+        '"obo:RO_0000052"': {'"owl:ObjectProperty"'},
+    }
+
+    # m = wiring_rs.extract_types(
+    #     "path/to/rdf-graph"
+    # )
+
+    ofn = (
+        '["SubClassOf","obo:OBI_2100378",'
+        '["ObjectIntersectionOf",'
+        '["SomeValuesFrom",'  # not typed
+        '"obo:BFO_0000055",'
+        '["ObjectIntersectionOf","obo:OBI_0000275",'
+        '["SomeValuesFrom","obo:RO_0000052","obo:CHEBI_33262"]]],'  # not typed
+        '["SomeValuesFrom","obo:OBI_0000293","obo:CHEBI_33262"]]]'  # not typed
+    )
+
+    print("======")
+    print("DEMO: Typing of OFN-S expression")
+    print("--------------------------------")
+    print("Input: ")
+    print(ofn)
+    print("Output: ")
+    print(
+        wiring_rs.ofn_typing(
+            ofn,
+            m,
+        )
+    )
+    print("======")
+
+
+# def extract_labeling():
+#    print(
+#        wiring_rs.extract_labels(
+#            "path/to/rdf-graph"
+#        )
+#    )
+
+
+def labeling_demo():
+    m = {
+        '"obo:CHEBI_33262"': '"test_label"',
+        '"obo:RO_0000052"': '"test_label_2"',
+    }
+
+    # get label information from file
+    # m = wiring_rs.extract_labels(
+    #     "/home/chris/knocean/projects/wiring-py/resources/thickOBI.txt"
+    # )
+    ofn = (
+        '["SubClassOf","obo:OBI_2100378",'
+        '["ObjectIntersectionOf",'
+        '["ObjectSomeValuesFrom",'
+        '"obo:BFO_0000055",'
+        '["ObjectIntersectionOf","obo:OBI_0000275",'
+        '["ObjectSomeValuesFrom","obo:RO_0000052","obo:CHEBI_33262"]]],'
+        '["ObjectSomeValuesFrom","obo:OBI_0000293","obo:CHEBI_33262"]]]'
+    )
+    print("======")
+    print("DEMO: Labeling of OFN-S expression")
+    print("----------------------------------")
+    print("Input: ")
+    print(ofn)
+    print("Output: ")
+    print(
+        wiring_rs.ofn_labeling(
+            ofn,
+            m,
+        )
+    )
+    print("======")
+
+
+def manchester_demo():
+    ofn = (
+        '["SubClassOf","obo:OBI_2100378",'
+        '["ObjectIntersectionOf",'
+        '["ObjectSomeValuesFrom",'
+        '"obo:BFO_0000055",'
+        '["ObjectIntersectionOf","obo:OBI_0000275",'
+        '["ObjectSomeValuesFrom","obo:RO_0000052","obo:CHEBI_33262"]]],'
+        '["ObjectSomeValuesFrom","obo:OBI_0000293","obo:CHEBI_33262"]]]'
+    )
+
+    print("======")
+    print("DEMO: OFN-S expression to Manchester")
+    print("------------------------------------")
+    print("Input: ")
+    print(ofn)
+    print("Output: ")
+    print(wiring_rs.ofn_2_man(ofn))
+
+    m = wiring_rs.extract_labels(
+        "/home/chris/knocean/projects/wiring-py/resources/thickOBI.txt"
+    )
+
+    labeled = wiring_rs.ofn_labeling(ofn, m)
+    print(wiring_rs.ofn_2_man(labeled))
+    print("======")
+
+
+def ofn_2_thick_demo():
+    ofn = (
+        '["SubClassOf","obo:OBI_2100378",'
+        '["ObjectIntersectionOf",'
+        '["ObjectSomeValuesFrom",'
+        '"obo:BFO_0000055",'
+        '["ObjectIntersectionOf","obo:OBI_0000275",'
+        '["ObjectSomeValuesFrom","obo:RO_0000052","obo:CHEBI_33262"]]],'
+        '["ObjectSomeValuesFrom","obo:OBI_0000293","obo:CHEBI_33262"]]]'
+    )
+
+    print("======")
+    print("DEMO: OFN-S expression to Thick Triple")
+    print("--------------------------------------")
+    print("Input: ")
+    print(ofn)
+    print("Output: ")
+    print(wiring_rs.ofn_2_thick(ofn))
+    print("======")
+
+
+if __name__ == "__main__":
+    thick_2_ofn_demo()
+    print("")
+    typing_demo()
+    print("")
+    labeling_demo()
+    print("")
+    manchester_demo()
+    print("")
+    ofn_2_thick_demo()
