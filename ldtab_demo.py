625f530838047ebaa5a3d0e864a24bb5972d9e74
chris <chris.kind.man@gmail.com>
2022-03-16 09:21:25 +0000

    Add example for `object2omn`

--- ldtab_demo.py
+++ ldtab_demo.py
@@ -3,6 +3,13 @@ import sqlite3
 import sys
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def get_types(connection, subject):
     cur = connection.cursor()
     query = (
@@ -12,7 +19,7 @@ def get_types(connection, subject):
     )
     types = set()
     for row in cur.execute(query):
-        types.add(row[5])
+        types.add(row["object"])
 
     return types
 
@@ -26,7 +33,7 @@ def get_labels(connection, subject):
     )
     label = set()
     for row in cur.execute(query):
-        label.add(row[5])
+        label.add(row["object"])
 
     return label
 
@@ -55,10 +62,35 @@ def get_labels_of_signature(connection, ofn):
     return label_map
 
 
-def get_statements(connection, subject):
+def get_statements(connection, table, subject):
+    connection.row_factory = dict_factory
     cur = connection.cursor()
-    query = f"SELECT * FROM statement WHERE subject='{subject}'"
+    query = f"SELECT * FROM {table} WHERE subject='{subject}'"
     return cur.execute(query)
+
+
+def object2omn(connection, table, json):
+    # 1. convert json to OFN (wiring.rs)
+    ofn = wiring_rs.object_2_ofn(json)
+    # 2. get types from database (wiring.py)
+    types = get_types_of_signature(connection, ofn)
+    # 3. get labels from database (wiring.py)
+    labels = get_labels_of_signature(connection, ofn)
+    # 4. typing (wiring.rs)
+    typed = wiring_rs.ofn_typing(ofn, types)
+    # 5. labelling (wiring.rs)
+    labeled = wiring_rs.ofn_labeling(typed, labels)
+    # 6. manchester string (wiring.rs)
+    man = wiring_rs.ofn_2_man(labeled)
+    return man
+
+
+def run_demo_2(database, subject):
+    con = sqlite3.connect(database, check_same_thread=False)
+    for row in get_statements(con, "statement", subject):
+        # check for _JSON
+        if row["datatype"] == "_JSON":
+            print(object2omn(con, "statement", row["object"]))
 
 
 def run_demo(database, subject):
@@ -67,14 +99,15 @@ def run_demo(database, subject):
     con = sqlite3.connect(database, check_same_thread=False)
 
     # query for ThickTriples of the subject
-    for row in get_statements(con, subject):
+    for row in get_statements(con, "statement", subject):
 
         # fetch data of a ThickTriple
-        subject = row[3]
-        predicate = row[4]
-        object = row[5]
+        subject = row["subject"]
+        predicate = row["predicate"]
+        object = row["object"]
 
         # convert ThickTriple to an OFN S-expression
+        # TODO provide support for datatypes
         ofn = wiring_rs.ldtab_2_ofn(subject, predicate, object)
 
         # fetch typing information relevant for the OFN S-exression
@@ -110,4 +143,5 @@ if __name__ == "__main__":
     database = sys.argv[1]
     subject = sys.argv[2]
 
-    run_demo(database, subject)
+    # run_demo(database, subject)
+    run_demo_2(database, subject)
